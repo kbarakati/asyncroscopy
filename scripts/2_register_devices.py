992a4ba97cc1625fadef0bfddca81971ca4f5227
ahoust17 <88668350+ahoust17@users.noreply.github.com>
2026-04-23 14:51:47 -0400

    abb notebook on database mode

--- scripts/2_register_devices.py
+++ scripts/2_register_devices.py
@@ -34,6 +34,10 @@ STAGE_SERVER  = "STAGE/stage_instance"
 STAGE_CLASS   = "STAGE" 
 STAGE_DEVICE  = "test/stage/1"
 
+CORRECTOR_SERVER  = "CORRECTOR/corrector_instance" 
+CORRECTOR_CLASS   = "CORRECTOR" 
+CORRECTOR_DEVICE  = "test/corrector/1"
+
 MICRO_SERVER  = "ThermoMicroscope/microscope_instance"
 MICRO_CLASS   = "ThermoMicroscope"
 MICRO_DEVICE  = "test/microscope/1"
@@ -57,17 +61,20 @@ def main():
     add_device(db, CAMERA_SERVER, CAMERA_CLASS, CAMERA_DEVICE)
     add_device(db, EDS_SERVER, EDS_CLASS, EDS_DEVICE)
     add_device(db, STAGE_SERVER, STAGE_CLASS, STAGE_DEVICE)
+    add_device(db, CORRECTOR_SERVER, CORRECTOR_CLASS, CORRECTOR_DEVICE)
     add_device(db, MICRO_SERVER, MICRO_CLASS, MICRO_DEVICE)
 
     db.put_device_property(MICRO_DEVICE, {"scan_device_address": [SCAN_DEVICE]})
     db.put_device_property(MICRO_DEVICE, {"camera_device_address": [CAMERA_DEVICE]})
     db.put_device_property(MICRO_DEVICE, {"eds_device_address": [EDS_DEVICE]})
     db.put_device_property(MICRO_DEVICE, {"stage_device_address": [STAGE_DEVICE]})
+    db.put_device_property(MICRO_DEVICE, {"corrector_device_address": [CORRECTOR_DEVICE]})
 
     print(f"  property:   scan_device_address = {SCAN_DEVICE}")
     print(f"  property:   camera_device_address = {CAMERA_DEVICE}")
     print(f"  property:   eds_device_address = {EDS_DEVICE}")
     print(f"  property:   stage_device_address = {STAGE_DEVICE}")
+    print(f"  property:   corrector_device_address = {CORRECTOR_DEVICE}")
 
     print("\nDone!")
 
